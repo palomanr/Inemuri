f88111aab391a74667254594e3a36a839eb3348c
Paloma Núñez Reyes <palomanunezreyes@gmail.com>
2023-02-20 12:13:50 +0100

    conectar toda la pagina
    clickon events

---
+++ saber_mas.js
@@ -0,0 +1,3 @@
+function clickinicio(target) {
+    location.href='index.html';
+};
---
+++ script.js
@@ -0,0 +1,7 @@
+function clickinner(target) { // Target refers to the clicked element
+    location.href='no_inicia_sesion.html';
+};
+function clicksabermas(target) {
+    location.href='saber_mas.html';
+};
+      
